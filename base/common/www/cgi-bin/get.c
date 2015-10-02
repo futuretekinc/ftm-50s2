95ff1512823356e32f584466e80dff7f90e8098e
Inhyun Cho <xtra72@gmail.com>
2015-10-02 15:41:31 +0900

    make up base

--- base/common/www/cgi-bin/get.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-main()
-{
-     printf("Content-type: text/html\n\n");
-          printf(" INPUT <HR>\n");
-               printf("%s", getenv("QUERY_STRING"));
-               }
